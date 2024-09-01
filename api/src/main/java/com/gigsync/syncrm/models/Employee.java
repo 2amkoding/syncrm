a994b34689ebc1988402b6b6c4ad060d074d666f
2amkoding <2amkoding@gmail.com>
2024-09-01 17:38:58 -0500

    updated dependencies so server stays running

--- api/src/main/java/com/gigsync/syncrm/models/Employee.java
+++ api/src/main/java/com/gigsync/syncrm/models/Employee.java
@@ -1,4 +1,15 @@
 package com.gigsync.syncrm.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+
+@Entity
 public class Employee {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    protected Employee (){}
 }
