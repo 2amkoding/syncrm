d63c2822a8e13b727fd579152c81839180664fb8
2amkoding <2amkoding@gmail.com>
2024-09-05 18:31:35 -0500

    Initial commit, rebuilt project, gradle error

---
+++ syncrmAPI/src/main/java/com/gigsync/syncrmAPI/SyncrmApiApplication.java
@@ -0,0 +1,13 @@
+package com.gigsync.syncrmAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncrmApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncrmApiApplication.class, args);
+	}
+
+}
